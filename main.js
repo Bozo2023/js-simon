9171f233157811aaa8b6f3740e084480383df851
Bozo2023 <ulrob1.0@email.com>
2024-01-22 06:34:31 +0100

    end

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const domaniHtml= document.getElementById("Domani")
 // Imposta la data di fine conto alla rovescia
-let Domani = new Date("Jan 19, 2024 09:30:00").getTime();
+const Domani = new Date("Jan 19, 2024 09:30:00").getTime();
 
 // Aggiorna il conto alla rovescia ogni secondo
 let rovescia = setInterval(function() {
 
   // Ottieni la data e l'ora corrente
-  let adesso = new Date("Jan 18, 2024 23:00:00").getTime();
+  const adesso = new Date().getTime();
 
   // Trova la distanza tra ora corrente e la data di fine conto alla rovescia
   let distance = Domani - adesso;
   
 
   // Calcola i giorni, le ore, i minuti e i secondi rimanenti
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
